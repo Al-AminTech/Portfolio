1dccc48e0dc5659e55a02fd4f5ea78371da4b7e5
unknown <opemibilal@gmail.com>
2024-09-17 12:22:23 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Al-Amin Tech's Portfolio",
+  description: "Modern & Minimal Al-Amin Tech Portfolio",
 };
 
 export default function RootLayout({
@@ -30,6 +30,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        
       </body>
     </html>
   );
